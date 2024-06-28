20c7af07c6bc27c7a2cef0acc76e5d2e0bfb85ec
Mik-user <mikmikh@yandex.ru>
2024-06-28 12:26:04 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,9 @@
+#Домашнее задание "Динамическая типизация"
+name = 'Михаил'
+print ('Name:',name)
+age = 52
+print ('Age:',age)
+age = age+1
+print ('NewAge:',age)
+is_student = True
+print ('is_student:',is_student)
